ed125384069562d5084d7e6dbf16010d78e437c5
Siddesh Gandhe <s.gandhe@easternenterprise.com>
2025-06-26 09:02:19 +0530

    redux-persist related changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./store/store";
 import App from "./App";
+import { store, persistor } from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={<p>Loading...</p>} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,29 @@
+import { combineReducers } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
 import photoReducer from "../reducers/PhotoSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({
+  photos: photoReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["photos"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    photos: photoReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
